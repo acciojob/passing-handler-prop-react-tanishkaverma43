1babf7958cb74ae0c05b91bd5a69262592d1944f
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-04 16:54:25 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+// Selection.js
+import React from "react";
+import "../styles/App.css";
+
+
+
+
+function Selection({ color }) {
+  return (
+    <div 
+      className="fix-box" 
+      style={{ backgroundColor: color, width: "100px", height: "100px", border: "1px solid black" }}
+    >
+    </div>
+  );
+}
+
+export default Selection;
 
