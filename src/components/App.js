d8b3cff51308c2e5fadcfa2127abd3ccea92ea93
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-04 16:53:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,25 @@
+// App.js
 import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
+import ColourSelector from "./ColourSelector";
+import Selection from "./Selection";
 
-const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
+const colors = ["red", "blue", "green"];
 
-const title = 'Select the gradient and then the Box to change the color';
-
-const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
+function App() {
+  const [selectedColor, setSelectedColor] = useState("");
 
   return (
-    <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
-
-      <div className="row">
-        {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
-        ))}
+    <div>
+      <h1>Color Selector</h1>
+      <ColourSelector onSelectColor={setSelectedColor} colors={colors} />
+      <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
+        <Selection color={selectedColor} />
+        <Selection color={selectedColor} />
+        <Selection color={selectedColor} />
       </div>
-
-      <div className='row' id="children-wrapper">
-        {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
-          ))
-        }
-      </div>
-    </div >
-  )
+    </div>
+  );
 }
 
-
 export default App;
+
